b94326d036d28e85063ba031ce100b0ea55b9d89
DSH105 <jason.chirgwin@ozemail.com.au>
2013-09-30 13:10:35 +1000

    Fix incompatibility with other entity plugins to a certain extent

--- src/main/java/com/github/dsh105/echopet/entity/pet/CraftPet.java
+++ src/main/java/com/github/dsh105/echopet/entity/pet/CraftPet.java
@@ -6,7 +6,6 @@ import org.bukkit.craftbukkit.v1_6_R3.CraftServer;
 import org.bukkit.craftbukkit.v1_6_R3.entity.CraftCreature;
 import org.bukkit.craftbukkit.v1_6_R3.entity.CraftLivingEntity;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 
 public class CraftPet extends CraftCreature {
@@ -26,68 +25,6 @@ public class CraftPet extends CraftCreature {
 		return entityPet.getPet();
 	}
 
-	@Override
-	public EntityPet getHandle() {
-		return entityPet;
-	}
-
-	@Override
-	public EntityType getType() {
-		return getPet().getPetType().getEntityType();
-	}
-
-	@Override
-	public void damage(double amount) {
-		super.damage(amount);
-	}
-
-	@Override
-	public void damage(double amount, Entity entity) {
-		super.damage(amount);
-	}
-
-	@Override
-	public void setHealth(double health) {
-		super.setHealth(health);
-	}
-
-	@Override
-	public void setMaxHealth(double amount) {
-		super.setMaxHealth(amount);
-	}
-
-	public void _INVALID_damage(int amount) {
-		damage((double) amount);
-	}
-	
-	public void _INVALID_damage(int amount, Entity source) {
-		damage((double) amount, source);
-	}
-	
-	public int _INVALID_getHealth() {
-		return (int) getHealth();
-	}
-	
-	public void _INVALID_setHealth(int health) {
-		setHealth((double) health);
-	}
-
-	public int _INVALID_getMaxHealth() {
-		return (int) getMaxHealth();
-	}
-	
-	public void _INVALID_setMaxHealth(int health) {
-		setMaxHealth((double) health);
-	}
-
-	public int _INVALID_getLastDamage() {
-		return (int) getLastDamage();
-	}
-
-	public void _INVALID_setLastDamage(int damage) {
-		setLastDamage((double) damage);
-	}
-
 	public CraftLivingEntity getTarget() {
 		if (getHandle().target == null) return null;
 		if (!(getHandle().target instanceof EntityLiving)) return null;
@@ -104,4 +41,55 @@ public class CraftPet extends CraftCreature {
 			entity.pathEntity = entity.world.findPath(entity, entity.target, 16.0F, true, false, false, true);
 		}
 	}
+
+	@Override
+	public void _INVALID_damage(int amount) {
+		damage((double) amount);
+	}
+
+	@Override
+	public void _INVALID_damage(int amount, Entity source) {
+		damage((double) amount, source);
+	}
+
+	@Override
+	public int _INVALID_getHealth() {
+		return (int) getHealth();
+	}
+
+	@Override
+	public int _INVALID_getLastDamage() {
+		return (int) getLastDamage();
+	}
+
+	@Override
+	public int _INVALID_getMaxHealth() {
+		return (int) getMaxHealth();
+	}
+
+	@Override
+	public void _INVALID_setHealth(int health) {
+		setHealth((double) health);
+	}
+
+	@Override
+	public void _INVALID_setLastDamage(int damage) {
+		setLastDamage((double) damage);
+	}
+
+	@Override
+	public void _INVALID_setMaxHealth(int health) {
+	}
+
+	@Override
+	public void setHealth(double health) {
+		if (health < 0) {
+			health = 0;
+		}
+		if (health > getMaxHealth()) {
+			health = getMaxHealth();
+		}
+		super.setHealth(health);
+	}
+
 }
