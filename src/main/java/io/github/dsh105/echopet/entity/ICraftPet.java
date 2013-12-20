0ad633c661180051bb05326127effd529f1fb7c8
DSH105 <jason.chirgwin@icloud.com>
2013-12-20 13:23:19 +1100

    Pets are recognised as Vehicles. Fixes #117

--- src/main/java/io/github/dsh105/echopet/entity/ICraftPet.java
+++ src/main/java/io/github/dsh105/echopet/entity/ICraftPet.java
@@ -1,8 +1,9 @@
 package io.github.dsh105.echopet.entity;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Vehicle;
 
-public interface ICraftPet extends Entity {
+public interface ICraftPet extends Entity, Vehicle {
 
     public void remove();
 
