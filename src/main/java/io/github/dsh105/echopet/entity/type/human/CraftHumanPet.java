a6bad3ff3a1be0ab95df631b5521fd93c5dc7c71
DSH105 <jason.chirgwin@icloud.com>
2014-02-10 19:48:10 +1100

    Fix crashes with Human Pets

--- src/main/java/io/github/dsh105/echopet/entity/type/human/CraftHumanPet.java
+++ src/main/java/io/github/dsh105/echopet/entity/type/human/CraftHumanPet.java
@@ -1,15 +1,12 @@
 package io.github.dsh105.echopet.entity.type.human;
 
-import io.github.dsh105.echopet.entity.CraftPet;
-import io.github.dsh105.echopet.entity.EntityPacketPet;
-import io.github.dsh105.echopet.entity.EntityPetType;
-import io.github.dsh105.echopet.entity.PetType;
+import io.github.dsh105.echopet.entity.*;
 import org.bukkit.craftbukkit.v1_7_R1.CraftServer;
 
 @EntityPetType(petType = PetType.HUMAN)
 public class CraftHumanPet extends CraftPet {
 
-    public CraftHumanPet(CraftServer server, EntityPacketPet entity) {
+    public CraftHumanPet(CraftServer server, EntityPet entity) {
         super(server, entity);
     }
 }
