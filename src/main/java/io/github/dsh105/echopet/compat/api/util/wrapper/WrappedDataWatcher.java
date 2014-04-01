469748ebc1f421af31e76ff8b0ba55056aa73c1b
DSH105 <jason.chirgwin@icloud.com>
2014-04-01 20:11:00 +1100

    Fix license issues

--- src/main/java/io/github/dsh105/echopet/compat/api/util/wrapper/WrappedDataWatcher.java
+++ src/main/java/io/github/dsh105/echopet/compat/api/util/wrapper/WrappedDataWatcher.java
@@ -15,23 +15,6 @@
  * along with EchoPet.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This file is part of HoloAPI.
- *
- * HoloAPI is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * HoloAPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with HoloAPI.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package io.github.dsh105.echopet.compat.api.util.wrapper;
 
 import io.github.dsh105.echopet.compat.api.plugin.EchoPet;
