d50741d510da15c5a6f19254f5d3da6aed45da85
DSH105 <jason.chirgwin@icloud.com>
2014-07-13 13:18:30 +1000

    Implement missing NMS entity types

---
+++ modules/v1_7_R2/src/main/java/com/dsh105/echopetv3/nms/v1_7_R2/entity/type/EntitySilverfishPetBase.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of EchoPet.
+ *
+ * EchoPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EchoPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EchoPet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.echopetv3.nms.v1_7_R2.entity.type;
+
+import com.dsh105.echopetv3.api.entity.entitypet.type.EntitySilverfishPet;
+import com.dsh105.echopetv3.api.entity.pet.type.SilverfishPet;
+import com.dsh105.echopetv3.nms.v1_7_R2.entity.EntityPetBase;
+import net.minecraft.server.v1_7_R2.World;
+
+public class EntitySilverfishPetBase extends EntityPetBase<SilverfishPet> implements EntitySilverfishPet {
+
+    public EntitySilverfishPetBase(World world, SilverfishPet pet) {
+        super(world, pet);
+    }
+}
