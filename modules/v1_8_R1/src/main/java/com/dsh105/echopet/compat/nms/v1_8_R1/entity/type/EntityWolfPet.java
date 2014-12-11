315ad278f0869e67bf81c1eecdf83392de269503
DSH105 <jason.chirgwin@icloud.com>
2014-12-11 22:19:40 +1100

    Apply visible changes to wolf status when the opposite is activated

--- modules/v1_8_R1/src/main/java/com/dsh105/echopet/compat/nms/v1_8_R1/entity/type/EntityWolfPet.java
+++ modules/v1_8_R1/src/main/java/com/dsh105/echopet/compat/nms/v1_8_R1/entity/type/EntityWolfPet.java
@@ -51,6 +51,7 @@ public class EntityWolfPet extends EntityAgeablePet implements IEntityWolfPet {
     public void setTamed(boolean flag) {
         if (isAngry() && flag) {
             this.getPet().getPetData().remove(PetData.ANGRY);
+            setAngry(false);
         }
 
         byte b0 = this.datawatcher.getByte(16);
@@ -66,6 +67,7 @@ public class EntityWolfPet extends EntityAgeablePet implements IEntityWolfPet {
     public void setAngry(boolean flag) {
         if (isTamed() && flag) {
             this.getPet().getPetData().remove(PetData.TAMED);
+            setTamed(false);
         }
 
         byte b0 = this.datawatcher.getByte(16);
