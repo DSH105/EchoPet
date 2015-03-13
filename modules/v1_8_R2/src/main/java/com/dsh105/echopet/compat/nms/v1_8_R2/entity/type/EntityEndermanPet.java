f434bfaa0a43b395febb018c28b0063dcf099862
DSH105 <jason.chirgwin@icloud.com>
2015-03-13 19:54:57 +1100

    Support MC-1.8.3, kinda.
    It compiles; that’s a plus.

---
+++ modules/v1_8_R2/src/main/java/com/dsh105/echopet/compat/nms/v1_8_R2/entity/type/EntityEndermanPet.java
@@ -0,0 +1,85 @@
+/*
+ * This file is part of EchoPet.
+ *
+ * EchoPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EchoPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EchoPet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.echopet.compat.nms.v1_8_R2.entity.type;
+
+import com.dsh105.echopet.compat.api.entity.*;
+import com.dsh105.echopet.compat.api.entity.type.nms.IEntityEndermanPet;
+import com.dsh105.echopet.compat.nms.v1_8_R2.entity.EntityPet;
+import net.minecraft.server.v1_8_R2.Block;
+import net.minecraft.server.v1_8_R2.World;
+
+@EntitySize(width = 0.6F, height = 2.9F)
+@EntityPetType(petType = PetType.ENDERMAN)
+public class EntityEndermanPet extends EntityPet implements IEntityEndermanPet {
+
+    public EntityEndermanPet(World world) {
+        super(world);
+    }
+
+    public EntityEndermanPet(World world, IPet pet) {
+        super(world, pet);
+    }
+
+    @Override
+    public void setScreaming(boolean flag) {
+        this.datawatcher.watch(18, Byte.valueOf((byte) (flag ? 1 : 0)));
+    }
+
+    @Override
+    protected void initDatawatcher() {
+        super.initDatawatcher();
+        this.datawatcher.a(16, new Short((short) 0));
+        this.datawatcher.a(17, new Byte((byte) 0));
+        this.datawatcher.a(18, new Byte((byte) 0));
+    }
+
+    @Override
+    protected String getIdleSound() {
+        return this.isScreaming() ? "mob.endermen.scream" : "mob.endermen.idle";
+    }
+
+    public boolean isScreaming() {
+        return this.datawatcher.getByte(18) > 0;
+    }
+
+    public void setCarried(Block block) {
+        this.datawatcher.watch(16, Short.valueOf((short) (Block.getId(block) & 255)));
+    }
+
+    public Block getCarried() {
+        return Block.getById(this.datawatcher.getShort(16));
+    }
+
+    public void setCarriedData(int i) {
+        this.datawatcher.watch(17, Byte.valueOf((byte) (i & 255)));
+    }
+
+    public int getCarriedData() {
+        return this.datawatcher.getByte(17);
+    }
+
+    @Override
+    protected String getDeathSound() {
+        return "mob.enderman.death";
+    }
+
+    @Override
+    public SizeCategory getSizeCategory() {
+        return SizeCategory.REGULAR;
+    }
+}
